6ad94f05152f73e11e0151f9520745340d02b188
Goncalini <goncalodaniel28@gmail.com>
2025-04-21 17:06:33 +0100

    feat: added project and theory

---
+++ Project/README.md
@@ -0,0 +1,1 @@
+# Cloud and Kubernetes Project
---
+++ Project/docker/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo ">>> Running migration"
+venv/bin/python manage.py migrate
+
+if [ "$SEED_DATABASE" == "true" ]; then
+  echo ">>> Running seed"
+  venv/bin/python seed.py
+fi
+
+echo ">>> Starting app..."
+venv/bin/python manage.py runserver 0.0.0.0:8000
